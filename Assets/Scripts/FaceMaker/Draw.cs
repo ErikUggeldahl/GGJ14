5d3b0538c725b8d57c064ad4d94edb0f3f93ed22
unknown <ugge0002@WJ316-02.ottawa.ad.algonquincollege.com>
2014-01-24 21:07:12 -0500

    Added template. Deleted Library.

---
+++ Assets/Scripts/FaceMaker/Draw.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draw : MonoBehaviour
+{
+	void Start()
+	{
+	}
+
+	void Update () 
+	{
+	}
+}
---
+++ Assets/Scripts/Template.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class T : MonoBehaviour
+{
+	void Start()
+	{
+	}
+	
+	void Update () 
+	{
+	}
+}
