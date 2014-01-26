cfc2502ee9305fd60617d0734eb0caf5a4a4153a
jeff0029 <jfvienneau@hotmail.com>
2014-01-25 23:25:52 -0500

    Added hud, health, Vassal Camera

---
+++ Assets/Scripts/GamePlay/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	public int damage;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			other.gameObject.GetComponent<TeamHealth>().TakeDamage(damage);
+			Destroy(gameObject, 0.3f);
+		}
+	}
+}
--- Assets/Scripts/Player/MoveCamera.cs
+++ Assets/Scripts/Player/MoveCamera.cs
@@ -8,19 +8,16 @@ public class MoveCamera : MonoBehaviour {
 	public Transform target;
 
 	float oldmouseX;
-	float oldmouseY;
 
 	float orbitDistance;
 
 	float mouseX;
-	float mouseY;
 	public float rotateSpeed;
 
 	// Use this for initialization
 	void Start () 
 	{
 		oldmouseX = Input.GetAxis ("Mouse X");
-		oldmouseY = Input.GetAxis ("Mouse Y");
 		orbitDistance = Vector3.Distance(camera.transform.position, target.transform.position);
 	}
 	
@@ -30,7 +27,6 @@ public class MoveCamera : MonoBehaviour {
 		mouseX = Input.GetAxis ("Mouse X");
 
 		float deltaMouseX = mouseX - oldmouseX;
-		float deltaMouseY = mouseY - oldmouseY;
 
 		transform.RotateAround(target.position, transform.up , deltaMouseX * rotateSpeed);
 	}
