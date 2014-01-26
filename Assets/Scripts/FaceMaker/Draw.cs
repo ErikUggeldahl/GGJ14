a82250a06097a58a541843557bc2c1759332abae
unknown <ugge0002@WJ316-02.ottawa.ad.algonquincollege.com>
2014-01-25 22:26:03 -0500

    Added menu for setup.
    Added system for transferring faces and names to the game.

--- Assets/Scripts/FaceMaker/Draw.cs
+++ Assets/Scripts/FaceMaker/Draw.cs
@@ -5,28 +5,55 @@ public class Draw : MonoBehaviour
 {
     public Material canvas;
     public Camera rayCam;
+
     int lastX = -1;
     int lastY = -1;
-    Texture2D tex;
 
-    public Texture Tex
-    {
-        get { return tex; }
-    }
+    Texture2D tex;
+    const int DIM = 128;
+    const int BRUSH_SIZE = 2;
+
+    bool drawing = false;
 
     void Start()
     {
-        tex = new Texture2D(128, 128);
+        tex = new Texture2D(DIM, DIM);
+        tex.filterMode = FilterMode.Point;
+        tex.alphaIsTransparency = true;
+
+        Color clear = new Color(1f, 1f, 1f, 0f);
+        Color[] clearArray = new Color[DIM * DIM];
+        for (int i = 0; i < clearArray.Length; i++)
+            clearArray[i] = clear;
+        tex.SetPixels(clearArray);
+    }
+
+    public void BeginDraw()
+    {
+        drawing = true;
+    }
+
+    public Texture2D EndDraw()
+    {
+        drawing = false;
+        return tex;
     }
 
     void Update()
     {
+        if (!drawing)
+            return;
+
         RaycastHit hit;
         if (!Physics.Raycast(rayCam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.5f)), out hit, float.PositiveInfinity, 1 << 8))
             return;
 
         int x = CanvasPtToPixel(hit.point.x);
+        if (x < 0 || x > DIM - BRUSH_SIZE)
+            return;
         int y = CanvasPtToPixel(hit.point.y);
+        if (y < 0 || y > DIM - BRUSH_SIZE)
+            return;
 
         if (lastX > 0 && Input.GetMouseButton(0))
             DrawLine(x, y, lastX, lastY);
@@ -38,14 +65,14 @@ public class Draw : MonoBehaviour
     int CanvasPtToPixel(float point)
     {
         point += 0.5f;
-        point *= 128f;
+        point *= (float)DIM;
         return (int)point;
     }
 
     void DrawAroundPoint(int x, int y)
     {
         Color c = Color.black;
-        tex.SetPixels(x, y, 2, 2, new Color[]{c, c, c, c});
+        tex.SetPixels(x, y, BRUSH_SIZE, BRUSH_SIZE, new Color[]{c, c, c, c});
     }
 
     void DrawLine(int x1, int y1, int x2, int y2)
